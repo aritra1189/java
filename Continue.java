50bea3dbc89194d095eb68eef7ef9a6c1a27b1c6
aritra sharma <98529719+aritra1189@users.noreply.github.com>
2022-11-16 20:04:04 +0530

    Add files via upload

---
+++ Continue.java
@@ -0,0 +1,17 @@
+public class Continue {
+    public static void main(String args[]){
+        
+            int j=1;
+            while(j<=6){
+                if(j==3){
+                    j++;
+                    continue;
+                }
+                System.out.println(j);
+                j++;
+            }
+        
+        }
+    }
+    
+
